c683dd0e3ebdfb3966d1f9f35c0990f5b310662a
ggazitbyul <ngusy123@gmail.com>
2022-11-07 13:34:43 +0900

    Application) 랜덤숫자 생성 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,29 @@
 package baseball;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static org.assertj.core.api.Assertions.assertThat;
+import camp.nextstep.edu.missionutils.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computerNumber = CreateRandomNumber();
+        System.out.println("숫자를 입력해주세요 : ");
+
+    }
+
+    private static List<Integer> CreateRandomNumber() {
+        List<Integer> randomNumberList = new ArrayList<>();
+        while (randomNumberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumberList.contains(randomNumber)) {
+                randomNumberList.add(randomNumber);
+            }
+        }
+        return randomNumberList;
     }
 }
