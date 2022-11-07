44aa7a9c0c6f75181783fb24d02ad50d233eb7bf
ggazitbyul <ngusy123@gmail.com>
2022-11-07 12:43:21 +0900

    README) 구현 기능 1번 진행 순서

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
