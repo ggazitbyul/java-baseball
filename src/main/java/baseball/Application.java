3403a021cb954485037d6136b28c6117723c7d2c
ggazitbyul <ngusy123@gmail.com>
2022-11-07 14:13:20 +0900

    Application) readUserNumber() 키보드 입력받은 문자열을 ArrayList로 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,26 +4,34 @@ import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> computerNumber = CreateRandomNumber();
+        List<String> computerNumber = createRandomNumber();
+        System.out.println(computerNumber); // 출력 확인용 임시
         System.out.println("숫자를 입력해주세요 : ");
-
+        List<String> userNumber = readUserNumber();
+        System.out.println(userNumber); // 출력 확인용 임시
     }
 
-    private static List<Integer> CreateRandomNumber() {
-        List<Integer> randomNumberList = new ArrayList<>();
+    private static List<String> createRandomNumber() {
+        List<String> randomNumberList = new ArrayList<>();
         while (randomNumberList.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            String randomNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
             if (!randomNumberList.contains(randomNumber)) {
                 randomNumberList.add(randomNumber);
             }
         }
         return randomNumberList;
     }
+
+    private static List<String> readUserNumber() {
+        String readWord = Console.readLine();
+        List<String> userNumberList = new ArrayList<>(List.of(readWord.split("")));
+
+        return userNumberList;
+    }
 }
+
